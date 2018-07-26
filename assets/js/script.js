0c253b4230c1e988b484dd38b1c3ae65563f53ac
Guillaume Ader <GAder@esriuk.com>
2018-07-26 19:55:26 +0100

    init

---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+/*-----------------
+	Components 
+-----------------*/
+
+var redditapi = "https://www.reddit.com/r/"
+
+// Parent | Subreddit component containing a list of 'post' components. 
+Vue.component('subreddit', {
+    props: ['name'],
+    data: function() {
+        return {
+            name: this.name,
+            posts: []
+        }
+    },
+    methods: {
+        fetchData: function() {
+            axios.get(redditapi + this.name + '.json')
+                .then(function(response) {
+                    this.posts = response.data.data.children
+                }.bind(this))
+        }
+    },
+    mounted() {
+        this.fetchData()
+    },
+    template: "#subreddit"
+})
+
+
+// Child | Componenet represiting a single post.
+
+Vue.component('post', {
+    props: ['item'],
+    methods: {
+        getImageBackgroundCSS: function(val) {
+            console.log(val)
+            return "background-image:url('" + val + "')"
+        }
+    },
+    template: "#post"
+})
+
+
+/*-----------------
+   Custom filters 
+-----------------*/
+
+
+// Filter that transform text to uppercase.
+Vue.filter('uppercase', function(value) {
+    return value.toUpperCase();
+});
+
+
+// Filter for cutting off strings that are too long.
+Vue.filter('truncate', function(value) {
+    var length = 60;
+
+    if (value.length <= length) {
+        return value;
+    } else {
+        return value.substring(0, length) + '...';
+    }
+});
+
+
+/*-----------------
+   Initialize app 
+-----------------*/
+
+new Vue({
+    el: '#main',
+    data: {
+
+        results: []
+    },
+    // mounted() {
+    //     axios.get(redditapi + this.name + ".json")
+    //         .then(response => {
+    //             this.results = response.data.results
+    //         })
+    // },
+    // template: "<div class='container'></div>"
+});
